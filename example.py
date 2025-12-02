842739c700d8f0152117da8da86beb379de64ee1
xwtan <tanxuewen10@gmail.com>
2025-12-02 20:30:33 +0800

    updated readme and output dir

--- example.py
+++ example.py
@@ -9,12 +9,10 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
 model = model.eval()
 
-# prompt = "<image>\nFree OCR. "
 prompt = "<image>\n<|grounding|>Convert the document to markdown. "
-#image_file = 'your_image.jpg'
 image_file = 'example.png'
 
-output_path = 'your/output/dir'
+output_path = 'output'
 
 # infer(self, tokenizer, prompt='', image_file='', output_path = ' ', base_size = 1024, image_size = 640, crop_mode = True, test_compress = False, save_results = False):
 
